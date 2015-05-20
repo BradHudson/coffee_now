75d11d6fa0b84a0190a6b37bdefd9b7feded56bf
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-05-20 15:50:04 -0400

    adding post

--- coffee.rb
+++ coffee.rb
@@ -4,15 +4,24 @@ module CoffeeNow
 	class CoffeeNowApp < Sinatra::Application	
 	  get '/' do
 		content_type :json
-		set_number_of_cups(99)
-		{ cups_ordered: get_order_status }.to_json
+		get_order_status
 	  end
+
+	  post '/order' do
+	  	set_number_of_cups(:num_cups)
+	  	set_name(:name)
+	  end
+
 	  def set_number_of_cups(cups)
 	  	@cups = cups
 	  end
+
+	  def set_name(name)
+	  	@name = name
+	  end
 	 
 	  def get_order_status()
-	  	@cups
+	  	{ cups_ordered: @cups, name: @name }.to_json
 	  end
 	end
 end
