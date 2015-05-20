08cb3f881db6dffbd40c3cb32ae85546161a7cac
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-05-20 13:12:35 -0400

    Sinatra Server and get Endpoint

--- coffee.rb
+++ coffee.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'json'
 
-get '/hi' do
-  "Hello World!"
+class CoffeeNow
+  def set_number_of_cups(cups)
+  	@cups = cups
+  end
+ 
+  def get_order_status()
+  	@cups
+  end
 end
+
+
+  get '/' do
+  content_type :json
+  instance = CoffeeNow.new
+  instance.set_number_of_cups(99)
+  { cups_ordered: instance.get_order_status }.to_json
+  end
